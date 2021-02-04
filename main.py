9472390b619230d5d3c5ef75adc61e81fb1f0970
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-02-04 21:19:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,90 @@
-import random, string
-import webbrowser
-import time
+from discord_webhook import DiscordWebhook
 import requests
+import random
+import string
+import time
+import os
 
-print(""" █████╗ ███╗   ██╗ ██████╗ ███╗   ██╗██╗██╗  ██╗
+class NitroGen: # Initialise the class
+    def __init__(self): # The initaliseaiton function
+        self.fileName = "Nitro Codes.txt" # Set the file name the codes are stored in
+
+    def main(self): # The main function contains the most important code
+        os.system('cls' if os.name == 'nt' else 'clear') # Clear the screen
+
+        print(""" █████╗ ███╗   ██╗ ██████╗ ███╗   ██╗██╗██╗  ██╗
 ██╔══██╗████╗  ██║██╔═══██╗████╗  ██║██║╚██╗██╔╝
-███████║██╔██╗ ██║██║   ██║██╔██╗ ██║██║ ╚███╔╝ 
-██╔══██║██║╚██╗██║██║   ██║██║╚██╗██║██║ ██╔██╗ 
+███████║██╔██╗ ██║██║   ██║██╔██╗ ██║██║ ╚███╔╝
+██╔══██║██║╚██╗██║██║   ██║██║╚██╗██║██║ ██╔██╗
 ██║  ██║██║ ╚████║╚██████╔╝██║ ╚████║██║██╔╝ ██╗
 ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
-                                                """)
-time.sleep(2)
-print("Ay brudda")
-time.sleep(0.3)
-print("Subscribe to BEN KOF FINAL, will appreciate.\n")
-time.sleep(0.2)
+                                                        """) # Print the title card
+        time.sleep(2) # Wait a few seconds
+        self.slowType("Made by: Drillenissen#4268 && Benz#4947", .02) # Print who developed the code
+        time.sleep(1) # Wait a little more
+        self.slowType("\nInput How Many Codes to Generate and Check: ", .02, newLine = False) # Print the first question
 
-num=input('Input How Many Codes to Generate and Check: ')
+        num = int(input('')) # Ask the user for the amount of codes
 
-f=open("Nitro Codes.txt","w", encoding='utf-8')
+        # Get the webhook url, if the user does not wish to use a webhook the message will be an empty string
+        self.slowType("\nDo you wish to use a discord webhook? \nIf so type it here or press enter to ignore: ", .02, newLine = False)
+        url = input('') # Get the awnser
+        url = url if url != "" else None # If the url is empty make it be None insted
 
-print("Wait, Generating for you!")
-      
-for n in range(int(num)):
-   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
-   f.write('https://discord.gift/')
-   f.write(y)
-   f.write("\n")
+        self.generator(num) # Generate the nitro codes
+        self.checker(notify = url) # Check the codes
 
-f.close()
-
-#=============Checker=========================
+        input("\nThe end! Press Enter 5 times to close the program.") # Tell the user the program finished
+        [input(i) for i in range(4,0,-1)] # Wait for 4 enter presses
 
 
-with open("Nitro Codes.txt") as f:
-    for line in f:
-        nitro = line.strip("\n")
+    def slowType(self, text, speed, newLine = True): # Function used to print text a little more fancier
+        for i in text: # Loop over the message
+            print(i, end = "", flush = True) # Print the one charecter, flush is used to force python to print the char
+            time.sleep(speed) # Sleep a little before the next one
+        if newLine: # Check if the newLine argument is set to True
+            print() # Print a final newline to make it act more like a normal print statement
 
-        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+    def generator(self, amount): # Function used to generate and store nitro codes in a seperate file
+        with open(self.fileName, "w", encoding="utf-8") as file: # Load up the file in write mode
+            print("Wait, Generating for you") # Let the user know the code is generating the codes
 
-        r = requests.get(url)
+            start = time.time() # Note the initaliseation time
 
-        if r.status_code == 200:
-            print(" Valid | {} ".format(line.strip("\n")))
-            break
-        else:
-        	print(" Invalid | {} ".format(line.strip("\n")))
-input("The end! Press Enter 5 times to close the program.")
-input("4")
-input("3")
-input("2")
-input("1")
+            for i in range(amount): # Loop the amount of codes to generate
+                code = "".join(random.choices(
+                    string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                    k = 16
+                )) # Generate the code id
+
+                file.write(f"https://discord.gift/{code}\n") # Write the code
+
+            # Tell the user its done generating and how long tome it took
+            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n")
+
+    def checker(self, notify = None): # Function used to check nitro codes from a file
+        with open(self.fileName, "r", encoding="utf-8") as file: # Open the file containing the nitro codes
+            for line in file.readlines(): # Loop over each line in the file
+                nitro = line.strip("\n") # Remove the newline at the end of the nitro code
+
+                # Create the requests url for later use
+                url = f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+
+                response = requests.get(url) # Get the responce from the url
+
+                if response.status_code == 200: # If the responce went through
+                    print(f" Valid | {nitro} ") # Notify the user the code was valid
+
+                    if notify is not None: # If a webhook has been added
+                        DiscordWebhook(
+                            url = notify,
+                            content = f"Valid Nito Code detected! @everyone \n{nitro}"
+                        ).execute() # Send the message to discord letting the user know there has been a valid nitro code
+                    else: # If there has not been a discord webhook setup just stop the code
+                        break # Stop the loop since a valid code was found
+                else: # If the responce got ignored or is invalid ( such as a 404 or 405 )
+                	print(f" Invalid | {nitro} ") # Tell the user it tested a code and it was invalid
+
+if __name__ == '__main__':
+    Gen = NitroGen() # Create the nitro generator object
+    Gen.main() # Run the main code
