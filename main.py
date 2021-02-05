13efee6b5c4513514e9fd2ca14f0e2b374073954
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-02-05 23:15:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class NitroGen: # Initialise the class
 ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
                                                         """) # Print the title card
         time.sleep(2) # Wait a few seconds
-        self.slowType("NITRO GENERATOR V2 Made by: Drillenissen#4268 && Benz#4947", .02) # Print who developed the code
+        self.slowType("Made by: Drillenissen#4268 && Benz#4947", .02) # Print who developed the code
         time.sleep(1) # Wait a little more
         self.slowType("\nInput How Many Codes to Generate and Check: ", .02, newLine = False) # Print the first question
 
@@ -29,16 +29,36 @@ class NitroGen: # Initialise the class
         # Get the webhook url, if the user does not wish to use a webhook the message will be an empty string
         self.slowType("\nDo you wish to use a discord webhook? \nIf so type it here or press enter to ignore: ", .02, newLine = False)
         url = input('') # Get the awnser
-        url = url if url != "" else None # If the url is empty make it be None insted
+        webhook = url if url != "" else None # If the url is empty make it be None insted
 
-        self.generator(num) # Generate the nitro codes
-        result = self.checker(notify = url) # Check the codes
+        print() # Print a newline for looks
+
+        valid = [] # Keep track of valid codes
+        invalid = 0 # Keep track of how many invalid codes was detected
+
+        for i in range(num): # Loop over the amount of codes to check
+            code = "".join(random.choices( # Generate the id for the gift
+                string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                k = 16
+            ))
+            url = f"https://discord.gift/{code}" # Generate the url
+
+            result = self.quickChecker(url, webhook) # Check the codes
+
+            if result: # If the code was valid
+                valid.append(url) # Add that code to the list of found codes
+            else: # If the code was not valid
+                invalid += 1 # Increase the invalid counter by one
+
+            if result and webhook is None: # If the code was found and the webhook is not setup
+                break # End the script
+
 
         print(f"""
 Results:
- Valid: {len(result['valid'])}
- Invalid: {result['invalid']}
- Valid Codes: {', '.join(result['valid'])}""") # Give a report of the results of the check
+ Valid: {len(valid)}
+ Invalid: {invalid}
+ Valid Codes: {', '.join(valid )}""") # Give a report of the results of the check
 
         input("\nThe end! Press Enter 5 times to close the program.") # Tell the user the program finished
         [input(i) for i in range(4,0,-1)] # Wait for 4 enter presses
@@ -66,9 +86,9 @@ Results:
                 file.write(f"https://discord.gift/{code}\n") # Write the code
 
             # Tell the user its done generating and how long tome it took
-            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n")
+            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n") #
 
-    def checker(self, notify = None): # Function used to check nitro codes from a file
+    def fileChecker(self, notify = None): # Function used to check nitro codes from a file
         valid = [] # A list of the valid codes
         invalid = 0 # The amount of invalid codes detected
         with open(self.fileName, "r", encoding="utf-8") as file: # Open the file containing the nitro codes
@@ -85,10 +105,10 @@ Results:
                     valid.append(nitro) # Append the nitro code the the list of valid codes
 
                     if notify is not None: # If a webhook has been added
-                        DiscordWebhook(
+                        DiscordWebhook( # Send the message to discord letting the user know there has been a valid nitro code
                             url = notify,
                             content = f"Valid Nito Code detected! @everyone \n{nitro}"
-                        ).execute() # Send the message to discord letting the user know there has been a valid nitro code
+                        ).execute()
                     else: # If there has not been a discord webhook setup just stop the code
                         break # Stop the loop since a valid code was found
 
@@ -97,6 +117,26 @@ Results:
                     invalid += 1 # Increase the invalid counter by one
 
         return {"valid" : valid, "invalid" : invalid} # Return a report of the results
+
+    def quickChecker(self, nitro, notify = None): # Used to check a single code at a time
+        # Generate the request url
+        url = f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+        response = requests.get(url) # Get the response from discord
+
+        if response.status_code == 200: # If the responce went through
+            print(f" Valid | {nitro} ") # Notify the user the code was valid
+
+            if notify is not None: # If a webhook has been added
+                DiscordWebhook( # Send the message to discord letting the user know there has been a valid nitro code
+                    url = notify,
+                    content = f"Valid Nito Code detected! @everyone \n{nitro}"
+                ).execute()
+
+            return True # Tell the main function the code was found
+
+        else: # If the responce got ignored or is invalid ( such as a 404 or 405 )
+            print(f" Invalid | {nitro} ") # Tell the user it tested a code and it was invalid
+            return False # Tell the main function there was not a code found
 
 if __name__ == '__main__':
     Gen = NitroGen() # Create the nitro generator object
