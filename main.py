87fedb21e1b9aacbbf3b589cbeaa526192bc34a1
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-02-09 12:30:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 class NitroGen: # Initialise the class
     def __init__(self): # The initaliseaiton function
         self.fileName = "Nitro Codes.txt" # Set the file name the codes are stored in
+        self.proxies = [] # The list of proxies
 
     def main(self): # The main function contains the most important code
         os.system('cls' if os.name == 'nt' else 'clear') # Clear the screen
@@ -23,7 +24,6 @@ class NitroGen: # Initialise the class
         self.slowType("Made by: Drillenissen#4268 && Benz#4947", .02) # Print who developed the code
         time.sleep(1) # Wait a little more
         self.slowType("\nInput How Many Codes to Generate and Check: ", .02, newLine = False) # Print the first question
-
         num = int(input('')) # Ask the user for the amount of codes
 
         # Get the webhook url, if the user does not wish to use a webhook the message will be an empty string
@@ -31,27 +31,67 @@ class NitroGen: # Initialise the class
         url = input('') # Get the awnser
         webhook = url if url != "" else None # If the url is empty make it be None insted
 
+        self.slowType("\nWant to use proxies? (Y/n): ", .02, newLine = False) # Ask if they want to use a proxy
+        proxy = input('') # Get the response
+
+        if "y" in proxy.lower(): # If the awnser was yes to the question above
+            self.proxySetup() # Trigger the proxy setup
+
         print() # Print a newline for looks
 
         valid = [] # Keep track of valid codes
         invalid = 0 # Keep track of how many invalid codes was detected
 
-        for i in range(num): # Loop over the amount of codes to check
-            code = "".join(random.choices( # Generate the id for the gift
-                string.ascii_uppercase + string.digits + string.ascii_lowercase,
-                k = 16
-            ))
-            url = f"https://discord.gift/{code}" # Generate the url
+        if "y" in proxy.lower():
+            total = 0
+            proxyIndx = 0
 
-            result = self.quickChecker(url, webhook) # Check the codes
+            while total < num:
+                try:
+                    while total < num:
+                        code = "".join(random.choices( # Generate the id for the gift
+                            string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                            k = 16
+                        ))
+                        url = f"https://discord.gift/{code}" # Generate the url
 
-            if result: # If the code was valid
-                valid.append(url) # Add that code to the list of found codes
-            else: # If the code was not valid
-                invalid += 1 # Increase the invalid counter by one
+                        result = self.quickProxyChecker(url, webhook, self.proxies[proxyIndx]) # Check the codes
 
-            if result and webhook is None: # If the code was found and the webhook is not setup
-                break # End the script
+                        if result == 200:
+                            print(f" Valid | {url} | {result}")
+
+                            if webhook is None:
+                                total = num + 1
+
+                        elif result == 429:
+                            print(f" Ratelimited | {self.proxies[proxyIndx]}")
+                            break
+                        elif result == 404:
+                            print(f" Invalid | {url} | {result}")
+                except Exception as e:
+                    print(e)
+
+                proxyIndx += 1
+                if proxyIndx == len(self.proxies):
+                    proxyIndx = 0
+
+        else:
+            for i in range(num): # Loop over the amount of codes to check
+                code = "".join(random.choices( # Generate the id for the gift
+                    string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                    k = 16
+                ))
+                url = f"https://discord.gift/{code}" # Generate the url
+
+                result = self.quickChecker(url, webhook) # Check the codes
+
+                if result: # If the code was valid
+                    valid.append(url) # Add that code to the list of found codes
+                else: # If the code was not valid
+                    invalid += 1 # Increase the invalid counter by one
+
+                if result and webhook is None: # If the code was found and the webhook is not setup
+                    break # End the script
 
 
         print(f"""
@@ -122,6 +162,7 @@ Results:
         # Generate the request url
         url = f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
         response = requests.get(url) # Get the response from discord
+        print(response.status_code)
 
         if response.status_code == 200: # If the responce went through
             print(f" Valid | {nitro} ") # Notify the user the code was valid
@@ -137,6 +178,68 @@ Results:
         else: # If the responce got ignored or is invalid ( such as a 404 or 405 )
             print(f" Invalid | {nitro} ") # Tell the user it tested a code and it was invalid
             return False # Tell the main function there was not a code found
+
+    def proxySetup(self):
+        self.slowType("\nDo you want to use \n1: Pre generated proxies\n2: A standalone proxylist?\n(1,2): ", .02, newLine = False) # Ask for the proxy list
+        awn = input('') # Get the awnser form the user
+
+        if awn == "1":
+            check = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1500&ssl=yes").text
+            responce = [i.strip() for i in check.strip().split("\n")]
+
+        elif awn == "2":
+            with open("proxy-list.txt", "w") as file:
+                file.write("")
+
+            self.slowType("\nPaste your list of proxies in the new file 'proxy-list.txt' where each proxy is on a new line, press enter once done", .02, newLine = False)
+            input("")
+
+            with open("proxy-list.txt", "r") as file:
+                responce = [i.strip() for i in file.read().strip().split("\n")]
+
+            os.remove("proxy-list.txt")
+
+            # if len(responce) > 100:
+            #     self.slowType("\nCannot accept more than 100 proxies at once", .02, newLine = False)
+            #     exit()
+
+        self.slowType("\nChecking proxy list, this might take a few minutes.\n", .02)
+
+        pos = 0
+        valid = 0
+        amount = len(responce)
+
+        for i in responce:
+            pos += 1
+            try:
+                check = requests.get("https://httpbin.org/ip", proxies = {"http" : f"http://{i}", "https" : f"https://{i}"}, timeout = 10)
+            except Exception as e:
+                # print(e)
+                print(f" {pos} / {amount} | Invalid | {i}")
+            else:
+                print(f" {pos} / {amount} | Valid   | {i}")
+                self.proxies.append(i)
+                valid += 1
+
+        self.slowType(f"\nChecked {amount} proxies with {valid} successes {valid / amount * 100}%", .02)
+
+    def quickProxyChecker(self, nitro, notify = None, proxy = None): # Used to check a single code at a time
+        # Generate the request url
+        url = f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+        response = requests.get(url, {"http" : f"http://{proxy}", "https" : f"https://{proxy}"}, timeout = 10) # Get the response from discord
+        print(response.status_code)
+
+        if response.status_code == 200: # If the responce went through
+            print(f" Valid | {nitro} ") # Notify the user the code was valid
+
+            if notify is not None: # If a webhook has been added
+                DiscordWebhook( # Send the message to discord letting the user know there has been a valid nitro code
+                    url = notify,
+                    content = f"Valid Nito Code detected! @everyone \n{nitro}"
+                ).execute()
+
+        return response.status_code # Tell the main function the status code for checking for ratelimiting
+
 
 if __name__ == '__main__':
     Gen = NitroGen() # Create the nitro generator object
