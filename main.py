49e36f93e7862e8e64a660efc5ca9a55abf99cfe
ShinKamiZ <99977035+ShinKamiZ@users.noreply.github.com>
2022-09-01 14:38:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class NitroGen:  # Initialise the class
                                                         """)  # Print the title card
         time.sleep(2)  # Wait a few seconds
         # Print who developed the code
-        self.slowType("Made by: Drillenissen#4268 && Benz#7274", .02)
+        self.slowType("Made by: Drillenissen#4268 && Benz#7274 and a bit fixed by Atmos#1480!", .02)
         time.sleep(1)  # Wait a little more
         # Print the first question
         self.slowType(
